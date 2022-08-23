abb5968bf5b5730b50a7ca423b8c80c8095820f2
MaryAnga <42803058+angamary@users.noreply.github.com>
2022-08-23 13:30:01 +0100

    Update RecommendationService.java
    MaryAnga-  changes made to recommendedAssets() method and getRecommendations()  methods. Sorted assets by gap between spare cash and asset cost

--- src/main/java/finance/simply/asset/recommender/service/RecommendationService.java
+++ src/main/java/finance/simply/asset/recommender/service/RecommendationService.java
@@ -16,53 +16,68 @@ import java.util.stream.Collectors;
 @Service
 public class RecommendationService {
 
-  private final DealRepository dealRepository;
+    private final DealRepository dealRepository;
 
-  private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-  private final AssetService assetService;
+    private final AssetService assetService;
 
-  @Autowired
-  public RecommendationService(DealRepository dealRepository, CustomerRepository customerRepository,
-                               AssetService assetService) {
-    this.dealRepository = dealRepository;
-    this.customerRepository = customerRepository;
-    this.assetService = assetService;
-  }
-
-  public Map<Customer, List<Asset>> getRecommendations(LocalDate date) {
-    List<Deal> deals = dealRepository.findAll();
-    List<Customer> customers = customerRepository.findAll();
-
-    Map<Customer, List<Asset>> recommendations = new HashMap<>();
-
-    for (Customer customer : customers) {
-      double currentPayment = 0.0;
-      List<Asset.Type> customerTypes = new ArrayList<>();
-
-      for (Deal deal : deals) {
-        if (deal.getCustomers().contains(customer)) {
-          if (!date.isBefore(deal.getStartDate()) && !date.isAfter(deal.getEndDate())) {
-            int numCustomers = deal.getCustomers().size();
-            double totalAssetCost =
-                    deal.getAssets().stream().reduce(0.0, (subTotal, asset) -> subTotal + asset.getCost(), Double::sum);
-            currentPayment += totalAssetCost / numCustomers;
-          }
-
-          customerTypes.addAll(deal.getAssets().stream().map(asset -> asset.getType()).collect(Collectors.toList()));
-        }
-      }
-
-      List<Asset> unsoldAssets = assetService.getUnsoldAssets();
-      for (Asset asset : unsoldAssets) {
-        if (asset.getCost() <= customer.getAffordability() - currentPayment &&
-            customerTypes.contains(asset.getType())) {
-          recommendations.computeIfAbsent(customer, cust -> new ArrayList<>()).add(asset);
-        }
-      }
+    @Autowired
+    public RecommendationService(DealRepository dealRepository, CustomerRepository customerRepository,
+                                 AssetService assetService) {
+        this.dealRepository = dealRepository;
+        this.customerRepository = customerRepository;
+        this.assetService = assetService;
     }
 
-    return recommendations;
-  }
+    public Map<Customer, List<Asset>> getRecommendations(LocalDate date) {
+        List<Deal> deals = dealRepository.findAll();
+        List<Customer> customers = customerRepository.findAll();
+
+        Map<Customer, List<Asset>> recommendations = new HashMap<>();
+
+        for (Customer customer : customers) {
+            double currentPayment = 0.0;
+            List<String> customerAssetTypes = new ArrayList<>();
+
+            for (Deal deal : deals) {
+                if (deal.getCustomers().contains(customer)) {
+                    if (!date.isBefore(deal.getStartDate()) && !date.isAfter(deal.getEndDate())) {
+                        int numCustomers = deal.getCustomers().size();
+                        double totalAssetCost =
+                                deal.getAssets().stream().reduce(0.0, (subTotal, asset) -> subTotal + asset.getCost(), Double::sum);
+                        currentPayment += totalAssetCost / numCustomers;
+                    }
+
+                    customerAssetTypes.addAll(deal.getAssets().stream().map(asset -> asset.getAssetType().getName()).collect(Collectors.toList()));
+                }
+            }
+
+            final double currentSpareCash = customer.getAffordability() - currentPayment;
+            recommendedAssets(recommendations, customer, customerAssetTypes, currentSpareCash);
+        }
+
+        return recommendations;
+    }
+
+    private void recommendedAssets(Map<Customer, List<Asset>> recommendations,
+                                   final Customer customer,
+                                   final List<String> customerTypes,
+                                   final double currentSpareCash) {
+        List<Asset> unsoldAssets = assetService.getUnsoldAssets();
+        for (Asset asset : unsoldAssets) {
+            if (asset.getCost() <= currentSpareCash &&
+                    customerTypes.contains(asset.getAssetType().getName())) {
+                recommendations.computeIfAbsent(customer, cust -> new ArrayList<>()).add(asset);
+            }
+        }
+
+        // Sort assets by gap between spare cash and asset cost
+        List<Asset> assets = recommendations.get(customer);
+        if (assets != null) {
+            List<Asset> sorted = assets.stream().sorted((o1, o2) -> (int) (o1.getCost() - currentSpareCash)).collect(Collectors.toList());
+            recommendations.put(customer, sorted);
+        }
+    }
 
 }
