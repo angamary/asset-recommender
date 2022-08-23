8492cbba3d1a0b6fa7717b343d55cab53f1020ac
MaryAnga <42803058+angamary@users.noreply.github.com>
2022-08-23 13:09:26 +0100

    Update Asset.java
    MaryAnga - Removed enum and made necessary changes

--- src/main/java/finance/simply/asset/recommender/model/Asset.java
+++ src/main/java/finance/simply/asset/recommender/model/Asset.java
@@ -6,81 +6,74 @@ import javax.persistence.*;
 @Table(name = "asset")
 public class Asset {
 
-  public enum Type {
-    AGRICULTURE,
-    TRANSPORT,
-    CONSTRUCTION,
-    WASTE
-  }
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-  @Id
-  @Column(name = "id")
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
+    @Column(name = "name")
+    private String name;
 
-  @Column(name = "name")
-  private String name;
+    @Column(name = "cost")
+    private double cost;
 
-  @Column(name = "cost")
-  private double cost;
+    @OneToOne
+    @JoinColumn(name = "type_id")
+    private AssetType assetType;
 
-  @Column(name = "type")
-  @Enumerated(EnumType.STRING)
-  private Type type;
+    @ManyToOne
+    @JoinColumn(name = "deal_id")
+    private Deal deal;
 
-  @ManyToOne
-  @JoinColumn(name = "deal_id")
-  private Deal deal;
+    public Asset() {
+    }
 
-  public Asset() {
-  }
+    public Asset(Integer id, String name, double cost, AssetType assetType, Deal deal) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.assetType = assetType;
+        this.deal = deal;
+    }
 
-  public Asset(Integer id, String name, double cost, Type type, Deal deal) {
-    this.id = id;
-    this.name = name;
-    this.cost = cost;
-    this.type = type;
-    this.deal = deal;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public AssetType getAssetType() {
+        return assetType;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public void setAssetType(AssetType assetType) {
+        this.assetType = assetType;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public double getCost() {
-    return cost;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setCost(double cost) {
-    this.cost = cost;
-  }
+    public double getCost() {
+        return cost;
+    }
 
-  public Type getType() {
-    return type;
-  }
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 
-  public void setType(Type type) {
-    this.type = type;
-  }
+    public Deal getDeal() {
+        return deal;
+    }
 
-  public Deal getDeal() {
-    return deal;
-  }
-
-  public void setDeal(Deal deal) {
-    this.deal = deal;
-  }
+    public void setDeal(Deal deal) {
+        this.deal = deal;
+    }
 
 }
