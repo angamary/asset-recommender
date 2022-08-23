6d4155564c3bfb48726167134844a29567d03690
MaryAnga <42803058+angamary@users.noreply.github.com>
2022-08-23 13:19:10 +0100

    Update DealRepository.java
    MaryAnga - Added findByStartDateLessThanAndEndDateGreaterThan() to DealRepository

--- src/main/java/finance/simply/asset/recommender/repository/DealRepository.java
+++ src/main/java/finance/simply/asset/recommender/repository/DealRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DealRepository extends JpaRepository<Deal, Integer> {}
+public interface DealRepository extends JpaRepository<Deal, Integer> {
+  public List<Deal> findByStartDateLessThanAndEndDateGreaterThan(LocalDate date,LocalDate date1);
+}
